bd8eab71d6d7ed51e0431b1db1c87a9e089ff640
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2025-04-03 16:55:23 +0100

    fix(proj1): fix minimax

--- proj1/src/game/AI.ts
+++ proj1/src/game/AI.ts
@@ -3,32 +3,24 @@ import { heuristics } from './heuristics';
 
 class Node {
   state: GameState;
-  move: GameMove;
+  move?: GameMove;
   value: number;
-  private children?: Node[];
 
   constructor(state: GameState, move?: GameMove) {
-    this.state = state;
-    this.move = move ?? new GameMove(-1, -1);
+    this.state = GameState.fromState(state);
+    this.move = move;
     this.value = 0;
   }
 
-  getChildren() {
-    return (this.children ??= this.state
-      .getValidMoves()
-      .map((move) => new Node(GameState.fromState(this.state), move)));
+  getChild(move: GameMove) {
+    const child = new Node(this.state, move);
+    child.state.makeMove(move.boardIndex, move.tileIndex);
+
+    return child;
   }
 
   isTerminal(): boolean {
-    return this.getChildren().length === 0;
-  }
-
-  static max(lhs: Node, rhs: Node): Node {
-    return lhs.value > rhs.value ? lhs : rhs;
-  }
-
-  static min(lhs: Node, rhs: Node): Node {
-    return lhs.value < rhs.value ? lhs : rhs;
+    return this.state.validMoves.length === 0;
   }
 }
 
@@ -73,12 +65,12 @@ export class GameAI {
    * @param state the game state
    * @returns a random valid move
    */
-  randomMove(state: GameState): GameMove {
-    const validMoves = state.getValidMoves();
+  private randomMove(state: GameState): GameMove {
+    const validMoves = state.validMoves;
     return validMoves[Math.floor(Math.random() * validMoves.length)];
   }
 
-  getMoveValue(state: GameState, move: GameMove) {
+  private getMoveValue(state: GameState, move: GameMove) {
     // apply the move
     state.makeMove(move.boardIndex, move.tileIndex);
 
@@ -97,39 +89,50 @@ export class GameAI {
   ): Node {
     // verify if the depth limit has been reached or the node is terminal
     if (depth === 0 || node.isTerminal()) {
-      node.value = this.getMoveValue(node.state, node.move);
+      node.value = this.getMoveValue(node.state, node.move!);
+      console.log(node);
       return node;
     }
 
+    console.log(`depth: ${depth}`);
+
     // define the algorithm variables
     let bestNode = new Node(node.state);
-    let evaluator: (lhs: Node, rhs: Node) => Node;
-    let pruner: (node: Node) => boolean;
+    let isBetterMove: (newValue: number, bestValue: number) => boolean;
+    let prune: (node: Node) => boolean;
 
     if (maximize) {
       bestNode.value = -Infinity;
-      evaluator = Node.max;
-      pruner = (node: Node) => {
+      isBetterMove = (newValue: number, bestValue: number) => {
+        return newValue > bestValue;
+      };
+      prune = (node: Node) => {
         alpha = Math.max(node.value, alpha);
-        return node.value > beta;
+        return node.value >= beta;
       };
     } else {
       bestNode.value = Infinity;
-      evaluator = Node.min;
-      pruner = (node: Node) => {
+      isBetterMove = (newValue: number, bestValue: number) => {
+        return newValue < bestValue;
+      };
+      prune = (node: Node) => {
         beta = Math.min(node.value, beta);
-        return node.value < alpha;
+        return node.value <= alpha;
       };
     }
 
-    // compute the best node
-    for (const child of node.getChildren()) {
-      bestNode = evaluator(
-        this.minimaxDFS(child, depth - 1, alpha, beta, !maximize),
-        bestNode,
-      );
+    // compute the best child node
+    for (const move of node.state.validMoves) {
+      const { value } = this.minimaxDFS(node.getChild(move), depth - 1, alpha, beta, !maximize);
 
-      if (pruner(bestNode)) {
+      // verify if a better move has been found
+      if (isBetterMove(value, bestNode.value)) {
+        bestNode.move = move;
+        bestNode.value = value;
+      }
+
+      // verify if we can prune the remaining child nodes
+      if (prune(bestNode)) {
         break;
       }
     }
@@ -138,7 +141,8 @@ export class GameAI {
   }
 
   minimax(state: GameState, depth: number): GameMove {
-    return this.minimaxDFS(new Node(state), depth, -Infinity, Infinity, true)
-      .move;
+    const move = this.minimaxDFS(new Node(state), depth, -Infinity, Infinity, true).move!;
+    console.log(move);
+    return move;
   }
 }
--- proj1/src/game/heuristics.ts
+++ proj1/src/game/heuristics.ts
@@ -16,7 +16,7 @@ export const heuristics = {
    * @returns true if the player has won the game, false otherwise
    */
   win: (state: GameState, move: GameMove): number => {
-    return Weight.Max * +state.checkWinner(state.board, move.player);
+    return Weight.Max * +(state.winner === move.player);
   },
   /**
    * Determines if the opponent can win the game in the next turn.
@@ -27,11 +27,8 @@ export const heuristics = {
   loss: (state: GameState, _: GameMove): number => {
     return (
       -Weight.Max *
-      +state.getValidMoves().some((move) => {
-        const nextState = GameState.fromState(state);
-        nextState.makeMove(move.boardIndex, move.tileIndex);
-
-        return nextState.checkWinner(nextState.board, state.nextPlayer);
+      +state.validMoves.some((move) => {
+        return GameState.fromState(state, move).winner === state.nextPlayer;
       })
     );
   },
@@ -39,7 +36,7 @@ export const heuristics = {
    * Determines if the player has won the small board they played on.
    * @param state the game state
    * @param move the move
-   * @returns true if the player has won the small board they played on, false otherwise
+   * @returns boolean indicating if the player has won the small board they played on
    */
   smallBoardWin: (state: GameState, move: GameMove): number => {
     return Weight.A_lot * +(state.board[move.boardIndex] === move.player);
@@ -48,22 +45,24 @@ export const heuristics = {
    * Determines if the opponent can win a small board in the next turn.
    * @param state the game state
    * @param move the move
-   * @returns true if the opponent can win a small board in the next turn, false otherwise
+   * @returns boolean indicating if the opponent can win a small board in the next turn
    */
   smallBoardLoss: (state: GameState, _: GameMove): number => {
     return (
       -Weight.A_lot *
-      +state.getValidMoves().some((move) => {
-        const nextState = GameState.fromState(state);
-        nextState.makeMove(move.boardIndex, move.tileIndex);
-
-        return state.board.some(
-          (winner, smallBoardIndex) =>
-            winner !== nextState.board[smallBoardIndex],
+      +state.validMoves.some((move) => {
+        return GameState.fromState(state, move).board.some(
+          (winner, smallBoardIndex) => winner !== state.board[smallBoardIndex],
         );
       })
     );
   },
+  /**
+   * Determines if the opponent will not be restricted to a small board in the next turn.
+   * @param state the game state
+   * @param _ the move
+   * @returns boolean indicating if the opponent will not be restricted to a small board in the next turn
+   */
   avoidFreeMove: (state: GameState, _: GameMove): number => {
     return -Weight.Average * +(state.nextBoardIndex < 0);
   },
@@ -71,7 +70,7 @@ export const heuristics = {
    * Determines if the current move blocks a diagonal occupied by the opponent.
    * @param state the game state
    * @param move the move
-   * @returns true if the current move blocks a diagonal occupied by the opponent, false otherwise
+   * @returns boolean indicating if the current move blocks a diagonal occupied by the opponent
    */
   blockOppositeCorner: (state: GameState, move: GameMove): number => {
     const smallBoard = state.smallBoards[move.boardIndex];
@@ -89,8 +88,7 @@ export const heuristics = {
     const index = corners.indexOf(move.tileIndex);
 
     return (
-      Weight.A_little *
-      +(index >= 0 && smallBoard[oppositeCorners[index]] === state.nextPlayer)
+      Weight.A_little * +(index >= 0 && smallBoard[oppositeCorners[index]] === state.nextPlayer)
     );
   },
   /**
